55e15f914d65501db7a565581b2edc85c4781472
Xknorthon <daniel.dev.duran@gmail.com>
2023-01-27 16:31:15 +0800

    create get computerChoice and playRound functions

---
+++ rpsgame.js
@@ -0,0 +1,29 @@
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+  
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = "rock";
+    console.log(computerSelection);
+    
+    
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+      } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+      ) {
+        return "You win! ";
+      } else {
+        return "You lose! ";
+      }
+    }
+   
+console.log(playRound("rock", "rock"));

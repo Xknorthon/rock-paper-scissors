74bb8e124bf48211702b2f2363ca9c036e04027d
Xknorthon <daniel.dev.duran@gmail.com>
2023-01-31 15:01:03 +0800

    finish project

--- rpsgame.js
+++ rpsgame.js
@@ -1,45 +1,41 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex].toLowerCase();
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice().toLowerCase();
-
-function playRound(playerSelection, computerSelection) {
-    
-    computerSelection = getComputerChoice().toLowerCase();
-    playerSelection = prompt("enter something").toLowerCase();
-    
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-      } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock")
-      ) {
-        return "You win! " + playerSelection + " beats " + computerSelection;
-      } else {
-        return "You lose! " + computerSelection + " beats " + playerSelection;
-    }
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
+  
+  if (playerSelection === computerSelection) {
+      return "It's a tie!";
+  } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+      return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+      return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
 }
-
-const playerSelection = prompt("Choose Rock, Paper or Scissors!!").toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
 
 function game() {
   let playerScore = 0;
   let computerScore = 0;
-  
+
   for (let i = 0; i < 5; i++) {
-    const result = playRound(playerSelection, computerSelection);
+    const playerSelection = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+    const result = playRound(playerSelection);
     console.log(result);
+    
+    if (result.startsWith("You win")) {
+      playerScore++;
+    } else if (result.startsWith("You lose")) {
+      computerScore++;
+    }
   }
-  
+
   if (playerScore > computerScore) {
     console.log("You win the game! Final score: " + playerScore + "-" + computerScore);
   } else if (playerScore < computerScore) {

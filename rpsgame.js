d4c0b5fb5728828fada4e669f9c42ecbc90d6841
Xknorthon <daniel.dev.duran@gmail.com>
2023-01-31 13:51:30 +0800

    add function game

--- rpsgame.js
+++ rpsgame.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     
     computerSelection = getComputerChoice().toLowerCase();
-    playerSelection = "rock".toLowerCase();
+    playerSelection = prompt("enter something").toLowerCase();
     
     if (playerSelection === computerSelection) {
         return "It's a tie!";
@@ -25,8 +25,28 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose Rock, Paper or Scissors!!").toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
 console.log(playerSelection);
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  
+  for (let i = 0; i < 5; i++) {
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+  }
+  
+  if (playerScore > computerScore) {
+    console.log("You win the game! Final score: " + playerScore + "-" + computerScore);
+  } else if (playerScore < computerScore) {
+    console.log("You lose the game! Final score: " + playerScore + "-" + computerScore);
+  } else {
+    console.log("The game is a tie! Final score: " + playerScore + "-" + computerScore);
+  }
+}
+
+game();
